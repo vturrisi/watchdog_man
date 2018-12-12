72a84bab6526eceea42b2379d46663cf4009e8af
Victor Turrisi <vt.turrisi@gmail.com>
2018-12-12 10:46:14 -0200

    updated basic description and setup files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # watchdog_man
+
+<div style="text-align:center"><img src="imgs/watchdog_man_colored.png" width="200" height="250">
---
+++ setup.py
@@ -0,0 +1,13 @@
+from numpy.distutils.core import setup
+
+
+setup(name='watchdog_man',
+      version='0.1.0',
+      description='Simple library to monitor experiments',
+      url='https://github.com/vturrisi/watchdog_man',
+      author='Victor Turrisi',
+      license='MIT',
+      packages=['watchdog_man'],
+      install_requires=[p.strip() for p in open('requirements.txt').readlines()],
+      include_package_data=True,
+      zip_safe=False)
